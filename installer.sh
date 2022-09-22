f1ee47ac4c1105923e4936e489b8911d8c03b8bb
zqpie <75803315+zqpie@users.noreply.github.com>
2022-09-22 16:50:58 -0400

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -3,7 +3,7 @@ mkdir ~/.config/i3
 mkdir ~/.config/kitty
 cp kitty.conf ~/.config/kitty/
 cp i3Conf ~/.config/i3/config
-mkdir ~/.conf/polybar
+mkdir ~/.config/polybar
 cp polyConf ~/.config/polybar/config.ini
 cp wallpaper.jpg ~/wallpaper.jpg
 sudo dnf update
