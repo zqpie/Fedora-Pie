96019ffd37d0f68345093ac8fe225a5a9f4eb88e
zqpie <75803315+zqpie@users.noreply.github.com>
2022-09-22 16:38:42 -0400

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,12 +1,13 @@
-mkdir .config
-mkdir .config/i3
-mkdir .config/kitty
-cp kitty.conf .config/kitty/
-cp i3Conf .config/i3/config
-mkdir .conf/polybar
-cp polyConf .config/polybar/config.ini
-dnf update
-dnf install levien-inconsolata-fonts feh nano sddm i3-gaps polybar kitty firefox rofi -y 
-systemctl set-default graphical.target
-systemctl enable sddm
+mkdir ~/.config
+mkdir ~/.config/i3
+mkdir ~/.config/kitty
+cp kitty.conf ~/.config/kitty/
+cp i3Conf ~/.config/i3/config
+mkdir ~/.conf/polybar
+cp polyConf ~/.config/polybar/config.ini
+cp wallpaper.jpg ~/wallpaper.jpg
+sudo dnf update
+sudo dnf install levien-inconsolata-fonts feh nano sddm i3-gaps polybar kitty firefox rofi -y 
+sudo systemctl set-default graphical.target
+sudo systemctl enable sddm
 
